7b3ba1fd9ab7472771521df676c1c4bf0440ccef
bulijojodibulido <2062898603@qq.com>
2019-04-04 15:07:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,38 @@
-# coding:utf-8
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
 
-from setuptools import setup
-# or
-# from distutils.core import setup  
+
+readme = open('README.md').read()
+
+VERSION = '1.0.0'
+
+requirements = [
+    'torch',
+]
 
 setup(
-        name='torchfp',     # 包名字
-        version='1.0',   # 包版本
-        description='This is a parameter and FLOPs calculation tools for pytorch model',   # 简单描述
-        author='tty',  # 作者
-        author_email='2062898603@qq.com',  # 作者邮箱
-        url='https://github.com/tirtile/torchFP/',      # 包的主页
-        packages=['torchFP'],                 # 包
+    # Metadata
+    name='torchfp',
+    version=VERSION,
+    author='tty',
+    author_email='',
+    url='https:/github.com/tirtile/torchFP/',
+    description='A tool to count the FLOPs of PyTorch model.',
+    long_description=readme,
+    license='MIT',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
 )
